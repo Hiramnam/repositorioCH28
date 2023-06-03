2e3e3cf2a022aeacc0c82d7e0370c3820e14cd2d
Hiramnam <hiramnm14@gmail.com>
2023-06-02 21:48:06 -0600

    Gimnasio Pokemon

---
+++ Variables_y_Datos/Variables-y-datos/script/script.js
@@ -0,0 +1,136 @@
+//Tipos de dayos y variables
+
+/*
+Declaración de variables
+
+var: utilizamos esta palabra reservada para variables globales = que se pueden usar a lo
+largo del código
+
+
+let: utilizamos esta palabra para variables locales 
+=se pueden utilizar en un bloque especifico de código
+
+
+
+const: esta palabra reservada la utilizamos para datos
+o información que no pueden cambiar su valor.
+
+var nombreDeVariable = x;
+
+
+
+*/
+
+//Tipo de datos númericos (Number)
+
+var edad =29;
+var altura = 1.75;
+var añoDeNacimiento = 1993;
+var diaDeLaSemana = 5;
+
+var edadTexto = "29";
+
+var nuevaEdad = Number(edadTexto);
+
+
+// Tipo de Dato cadena de texto (string)
+/* 
+Los tipos de datos string o cadenas de texto utilizan 
+comillas  dobles o sencillas para asignar un valor
+
+Son un tipo de datos sumamente flexible, sin embargo hay 
+que utilizarlos con cuidado 
+
+*/
+var nombre = "Hiram";
+var ciudad = "CDMX";
+var color = "azul";
+
+var edadString = String(edad);
+
+//Tipo de Dato Booleano (bolean)
+
+var esLagunero = true;
+var escuchaMusica = true;
+
+var condicion = Boolean(10<5);// da como resultado false
+
+
+// Tipos de datos booleanos (boolean)
+
+/* 
+Los tipos de datos Booleanos solo arrojan 2 resultados
+
+true
+
+false
+*/
+
+var nuevaVariable = null; 
+
+// Datos tipo Boolean
+
+var esMexicano = true;
+
+var esRockero = true;
+
+var tieneHambre = true;
+
+var esAlumno = true;
+ 
+var esMedico = false;
+ 
+var esChilango = true;
+
+//string
+
+var carrera = "Geofisica";
+
+var vivienda = "Edo Mex.";
+
+var comida = "Hamburguesa";
+
+var bebida = "limonada";
+
+var lugar = "Playa";
+
+var calzado = "botas";
+
+var vehiculo = "Camioneta";
+
+//Number
+
+var zapato = 9;
+
+var sillas = 4;
+
+var paresZapatos = 13;
+
+var comidasAlDia = 5;
+
+var amigos = 4;
+
+var tortas = 3; 
+
+//==========================================00
+
+const pi = 3.141592; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
